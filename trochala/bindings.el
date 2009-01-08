8116bc0ade9d9a715c0e7f5ccb3bf18dc3c9b79f
Christos Trochalakis <yatiohi@ideopolis.gr>
2009-01-08 11:03:32 +0200

    Key bindings: reset killing, turn off hippie-expand

--- trochala/bindings.el
+++ trochala/bindings.el
@@ -3,10 +3,10 @@
 (global-set-key (kbd "<f11>") 'dired); switch file
 (global-set-key (kbd "<f12>")  'djcb-full-screen-toggle)
 
-(global-set-key (kbd "M-/") 'hippie-expand)
-(global-set-key (kbd "C-w") 'backward-kill-word)
-(global-set-key (kbd "C-x C-k") 'kill-region)
-(global-set-key (kbd "C-c C-k") 'kill-region)
+;(global-set-key (kbd "M-/") 'hippie-expand)
+;(global-set-key (kbd "C-w") 'backward-kill-word)
+;(global-set-key (kbd "C-x C-k") 'kill-region)
+;(global-set-key (kbd "C-c C-k") 'kill-region)
 
 (global-set-key (kbd "C-c p") 'copy-line) ; copy current line
 (global-set-key (kbd "C-c k") 'kill-this-buffer)
