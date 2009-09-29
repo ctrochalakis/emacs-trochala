f99c2b9366c128e0a33e4318441b51f2cce62a89
Christos Trochalakis <yatiohi@ideopolis.gr>
2009-09-29 12:52:58 +0300

    custom: irc settings

---
+++ trochala/custom.el
@@ -0,0 +1,17 @@
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(browse-url-browser-function (quote browse-url-firefox))
+ '(erc-autojoin-channels-alist (quote (("freenode.net" "#emacs" "#django" "#transifex" "#git"))))
+ '(erc-email-userid "trochala")
+ '(erc-modules (quote (autojoin button completion fill irccontrols keep-place list match menu move-to-prompt netsplit networks noncommands readonly ring stamp track)))
+ '(erc-nick "trochala")
+ '(erc-user-full-name "Christos Trochalakis"))
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ )
