ed449f5c8283e43426967f5ac1e96f549ba01a95
Christos Trochalakis <yatiohi@ideopolis.gr>
2009-10-28 14:42:23 +0200

    escreen

--- trochala.el
+++ trochala.el
@@ -24,6 +24,7 @@
 (load "trochala/global")
 
 (load "trochala/trochala-python.el")
+(load "trochala/trochala-escreen.el")
 
 (yas/initialize)
 (yas/load-directory "~/.emacs.d/vendor/yasnippet/snippets/")
