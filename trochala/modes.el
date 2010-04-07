206527d910b24b7daf1ed298b6c55b2ae1f739a9
Christos Trochalakis <yatiohi@ideopolis.gr>
2010-04-07 20:55:07 +0300

    add coffee mode

--- trochala/modes.el
+++ trochala/modes.el
@@ -47,3 +47,6 @@
 ; Javascript
 (autoload 'js2-mode "js2" nil t)
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+(autoload 'coffee-mode "coffee-mode" "Mustache Mode" t)
+(add-to-list 'auto-mode-alist '("\\.coffee$" . coffee-mode))
+
