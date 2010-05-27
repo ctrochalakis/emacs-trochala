14bd21d8398806da543ee00073a6d7f7dd763183
Christos Trochalakis <yatiohi@ideopolis.gr>
2010-05-27 18:02:00 +0300

    markdown extention hooks

--- trochala/modes.el
+++ trochala/modes.el
@@ -40,6 +40,8 @@
 (autoload 'markdown-mode "markdown-mode"
    "Major mode for editing Markdown files" t)
 (add-to-list 'auto-mode-alist '("\\.mkd$" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.md$" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.markdown$" . markdown-mode))
 
 ; Egg python files
 ;; Use archive mode to open Python eggs
