decd14dfb8dd6f3a83f99f2dbbe7cb70ea58e454
Christos Trochalakis <yatiohi@ideopolis.gr>
2010-07-18 13:50:02 +0300

    nont set modes on trochala-ruby.el

--- trochala/trochala-ruby.el
+++ trochala/trochala-ruby.el
@@ -2,10 +2,4 @@
       (lambda () (local-set-key (kbd "RET") 'reindent-then-newline-and-indent)))
 (eval-after-load 'ruby-mode '(require 'rails-apidock))
 
-(setq auto-mode-alist
-      (append '(
-		("\\.rb$"   . ruby-mode)
-		("Rakefile" . ruby-mode)
-		("\\.rake$" . ruby-mode)
-;;		("\\.rjs"   . ruby-mode)
-		) auto-mode-alist))
+
