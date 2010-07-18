901c423c034085981fa6b057e0c61dfa8a0098af
Christos Trochalakis <yatiohi@ideopolis.gr>
2010-07-18 19:52:51 +0300

    new ruby-mode hooks, electric and stuff

--- trochala/trochala-ruby.el
+++ trochala/trochala-ruby.el
@@ -1,5 +1,15 @@
 (add-hook 'ruby-mode-hook
-      (lambda () (local-set-key (kbd "RET") 'reindent-then-newline-and-indent)))
-(eval-after-load 'ruby-mode '(require 'rails-apidock))
+      (lambda ()
+        (define-key ruby-mode-map (kbd "RET") 'reindent-then-newline-and-indent)
+        (add-hook 'write-file-functions
+                  '(lambda()
+                     (save-excursion
+                       (untabify (point-min) (point-max)))))
+        (set (make-local-variable 'indent-tabs-mode) 'nil)
+        (set (make-local-variable 'tab-width) 2)
+        (require 'ruby-electric)
+        (ruby-electric-mode t)))
 
+;; (eval-after-load 'ruby-mode '(require 'rails-apidock))
 
+(vendor 'ruby-hacks)
