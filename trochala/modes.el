656f21aef91b73420003454821c33be47748d94c
Christos Trochalakis <yatiohi@ideopolis.gr>
2010-07-19 10:06:16 +0300

    add tower files to ruby-mode

--- trochala/modes.el
+++ trochala/modes.el
@@ -74,6 +74,7 @@
 (add-to-list 'auto-mode-alist '("\\.task$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Rakefile" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Gemfile" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.tower$" . ruby-mode))
 
 (setq
  nxhtml-global-minor-mode t
