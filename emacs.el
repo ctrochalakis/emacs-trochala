1d1b4580b884afcf43e96666e089c1df6e2685bd
Christos Trochalakis <yatiohi@ideopolis.gr>
2011-07-22 18:18:11 +0300

    Switch package.el (elpa)

--- emacs.el
+++ emacs.el
@@ -5,12 +5,8 @@
 ;; so it won't be version-controlled. The idea is to
 ;; make this file load other version-controlled files.
 (load "local")
-;;; This was installed by package-install.el.
-;;; This provides support for the package system and
-;;; interfacing with ELPA, the package archive.
-;;; Move this code earlier if you want to reference
-;;; packages in your .emacs.
-(when
-    (load
-     (expand-file-name "~/.emacs.d/elpa/package.el"))
-  (package-initialize))
+
+(require 'package)
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(package-initialize)
+
